0d1a3ca6c8680b9ce43e66ccd0475f87e643aaf0
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2016-04-23 12:26:23 -0300

    Abstrai o codigo

---
+++ OJogo.py
@@ -0,0 +1,15 @@
+def recebe_jogada(linha,coluna):
+    
+    
+    
+def verifica_gnhador():
+    #len matriz
+    #verifica se há simbolos alinhados
+    if #nenhum alinhado:
+        return 0
+    elif #X alinhado:
+        return 1
+    elif #O alinhado:
+        return 2
+        
+    
